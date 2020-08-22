a258c7eed83ce6ebf0a24b983e23ce59b7731fa4
Kyien <kyien.njoroge@hotmail.com>
2020-08-22 12:38:16 +0300

    Update callback_url.php
    changed callback features back to they were initially

--- callback_url.php
+++ callback_url.php
@@ -15,60 +15,37 @@
         $transactionDate=$callbackData->Body->stkCallback->CallbackMetadata->Item[3]->Value;
         $phoneNumber=$callbackData->Body->stkCallback->CallbackMetadata->Item[4]->Value;
         
-
-        echo 'callback starting....';
-
-        echo $callbackData;
-    //    echo $phoneNumber;
-     //   $result=[
-     //       $transTime=>$transTime,
-      //      $transAmount=>$transAmount,
-        //    $businessShortCode=>$businessShortCode,
-          //  $billRefNumber=>$billRefNumber,
-            // $invoiceNumber=>$invoiceNumber,
-    //        $orgAccountBalance=>$orgAccountBalance,
-    //        $thirdPartyTransID=>$thirdPartyTransID,
-     //       $MSISDN=>$MSISDN,
-     //       $firstName=>$firstName,
-    //        $lastName=>$lastName,
-    //        $middleName=>$middleName,
-    //        $transID=>$transID,
-    //        $transactionType=>$transactionType
-
-    //    ];
-        
-    //       return json_encode($result);
-
+  
     
     
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-//$link = mysqli_connect("localhost", "ubunifu", "Ubunifu@20", "streams");
+$link = mysqli_connect("localhost", "ubunifu", "Ubunifu@20", "streams");
  
 // Check connection
-// if($link === false){
-//     die("ERROR: Could not connect. " . mysqli_connect_error());
-// }
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
  
 // Attempt insert query execution
-// $sql = "INSERT INTO mpesa_pay (
-// resultCode,
-// resultDesc,
-// merchantRequestID,
-// checkoutRequestID,
-// amount,
-// mpesaReceiptNumber,
-// balance,
-// transactionDate,
-// phoneNumber) VALUES ('$resultCode', '$resultDesc', '$merchantRequestID', '$checkoutRequestID', '$amount', '$mpesaReceiptNumber', '$balance','$transactionDate', '$phoneNumber' )";
-// if(mysqli_query($link, $sql)){
-//     echo "Records inserted successfully.";
-// } else{
-//     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-// }
+$sql = "INSERT INTO mpesa_pay (
+resultCode,
+resultDesc,
+merchantRequestID,
+checkoutRequestID,
+amount,
+mpesaReceiptNumber,
+balance,
+transactionDate,
+phoneNumber) VALUES ('$resultCode', '$resultDesc', '$merchantRequestID', '$checkoutRequestID', '$amount', '$mpesaReceiptNumber', '$balance','$transactionDate', '$phoneNumber' )";
+if(mysqli_query($link, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
 
 
- // sleep(100); // delay in seconds
+
 
 
 
@@ -77,38 +54,11 @@ server with default setting (user 'root' with no password) */
 
 
 // Close connection
-//mysqli_close($link);
+mysqli_close($link);
 
-/*
-$servername = "localhost";
-$username = "vanguar1_mpesa";
-$password = "mpesa1720!";
-$dbname = "vanguar1_mpesa";
+//  $stkCallbackResponse = file_get_contents('php://input');
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT merchantRequestID FROM mpesa_pay WHERE merchantRequestID = $merchantRequestID "; // merchant request id gotten from the first response variable
-$result = $conn->query($sql);
-
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "transaction was successful";
-  }
-} else {
-  echo "Transaction failed";
-}
- */
-
-//$stkCallbackResponse = file_get_contents('php://input');
-//file_put_contents('php://stderr', print_r($callbackData, TRUE))
-//   $logFile = "stkresponse.txt";
+//   $logFile = "stkPushCallbackResponse.json";
 //   $log = fopen($logFile, "a");
 //   fwrite($log, $callbackJSONData);
 //   fclose($log);
